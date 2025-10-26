9594e3b3d0ad42210905d48d2439a31dd2cc904e
ducduc1506 <vnduc1506@gmail.com>
2025-10-26 21:26:05 +0700

    first commit

---
+++ src/test/java/com/example/socialMediaBE/SocialMediaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialMediaBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
